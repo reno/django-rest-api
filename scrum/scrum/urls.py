784d69bee6eed22bf96748886aea441444cc628f
reno <renan.modenese@gmail.com>
2020-04-13 14:27:15 -0300

    Removendo referências ao Django Admin

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
 ]
