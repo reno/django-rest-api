40d9195d1073f28d1589b7ae34f4c1c33722e10a
reno <renan.modenese@gmail.com>
2020-04-14 15:26:22 -0300

    Atualizando urls

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+
 from rest_framework.authtoken.views import obtain_auth_token
+
+from board.urls import router
 
 
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/', include(router.urls)),
 ]
