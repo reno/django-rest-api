d7ef88d178692e7456bfda5c5d6ff421f49d18f8
reno <renan.modenese@gmail.com>
2020-04-14 16:38:59 -0300

    Incluindo filters

--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import get_user_model
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
 User = get_user_model()
 
-class DefaultsMixin(object):                                                
+
+class DefaultsMixin(object):
     """Default settings for view authentication, permissions,
     filtering and pagination."""
 
@@ -20,6 +22,11 @@ class DefaultsMixin(object):
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
 
 
 class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
@@ -27,13 +34,16 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
-
+    search_fields = ('name', )
+    ordering_fields = ('end', 'name', )
 
 class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for listing and creating tasks."""
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    search_fields = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
 
 class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
@@ -43,3 +53,4 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer
+    search_fields = (User.USERNAME_FIELD, )
