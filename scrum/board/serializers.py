58f7c20e8f1015e62540537a4cccba5f6c15ca24
reno <renan.modenese@gmail.com>
2020-04-14 16:14:10 -0300

    Corrigindo erro nos fields

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -9,11 +9,11 @@ User = get_user_model()
 
 class SprintSerializer(serializers.ModelSerializer):
 
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField(source='get_links')
 
     class Meta:
         model = Sprint
-        fields = ('id', 'name', 'description', 'end', )
+        fields = ('id', 'name', 'description', 'end', 'links')
 
     def get_links(self, obj):
         request = self.context['request']
@@ -28,13 +28,13 @@ class TaskSerializer(serializers.ModelSerializer):
     assigned = serializers.SlugRelatedField(slug_field=User.USERNAME_FIELD,
                                             required=False, read_only=True)
     status_display = serializers.SerializerMethodField('get_status_display')
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField(source='get_links')
 
     class Meta:
         model = Task
         fields = ('id', 'name', 'description', 'sprint',
                   'status', 'status_display', 'order',
-                  'assigned', 'started', 'due', 'completed', )
+                  'assigned', 'started', 'due', 'completed', 'links')
 
     def get_status_display(self, obj):
         return obj.get_status_display()
@@ -61,11 +61,11 @@ class TaskSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
 
     full_name = serializers.CharField(source='get_full_name', read_only=True)
-    links = serializers.SerializerMethodField('get_links')
+    links = serializers.SerializerMethodField(source='get_links')
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', 'links')
 
     def get_links(self, obj):
         request = self.context['request']
